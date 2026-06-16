0804809c68450d49fa06cc248dd8ee75962525ca
INSTHOLDERR <nikhilpramod425@gmail.com>
2026-06-16 23:51:45 +0530

    otp- setted

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -3,16 +3,12 @@ import jwt from "jsonwebtoken";
 export const generateToken = (userId, res) => {
   const { JWT_SECRET, NODE_ENV } = process.env;
 
-  const token = jwt.sign(
-    { userId },
-    JWT_SECRET,
-    { expiresIn: "7d" }
-  );
+  const token = jwt.sign({ userId }, JWT_SECRET, { expiresIn: "7d" });
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: NODE_ENV === "production" ? "none" : "strict",
     secure: NODE_ENV !== "development",
   });
 
